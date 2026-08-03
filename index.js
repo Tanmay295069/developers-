760dd53de54472e76adf3090cfab5b25d21e0766
yadnesh27407 <jyadnesh33@gmail.com>
2026-08-03 22:59:05 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+console.log(a)
+console.log(a+1)
+console.log(a+2)
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
